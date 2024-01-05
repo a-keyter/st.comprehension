64af49b3341ffc0c6a4597787e29323aa9111d4a
alexkeyter <keyteraj@cardiff.ac.uk>
2024-01-05 20:35:15 +0000

    Error catch, can't access trancscript

--- stream_app.py
+++ stream_app.py
@@ -158,129 +158,133 @@ elif generate and user_openai != "":
     language=lang_codes[target_lang],
   )
 
-  transcript = loader.load()
+  try:
+    transcript = loader.load()
+  except:
+      st.error("Transcript for this video is not available, please try a different video")
+
   title, thumbnail_url = get_youtube_details(user_link)
 
   st.subheader(title)
   st.image(thumbnail_url, caption='YouTube Video Thumbnail')
 
-  with st.expander("Transcript of video."):
-    st.write(transcript)
+  with st.spinner("Now creating your worksheet"):
+    # Get Key Vocab
+    vocab = key_vocab(vid_transcript=str(transcript[0].page_content), target_language=target_lang)
 
-  # Get Key Vocab
-  vocab = key_vocab(vid_transcript=str(transcript[0].page_content), target_language=target_lang)
+    vocab_list = vocab.split("#NEWTERM#")
 
-  vocab_list = vocab.split("#NEWTERM#")
+    def strip_enumeration(word_list):
+      return [word.split('. ', 1)[1] if '. ' in word else word for word in word_list]
 
-  def strip_enumeration(word_list):
-    return [word.split('. ', 1)[1] if '. ' in word else word for word in word_list]
+    vocab_list = strip_enumeration(vocab_list)
 
-  vocab_list = strip_enumeration(vocab_list)
+    with st.expander("Key Vocab"):
+      st.write(vocab_list)
 
-  with st.expander("Key Vocab"):
-    st.write(vocab_list)
+    # Generate Vocab Definitions
+    vocab_defs = def_vocab(vocab_list, target_lang)
+    vocab_defs.strip()
+    vocab_definition_list = vocab_defs.split("#NEWDEF#")
+    vocab_definition_list = [entry.strip() for entry in vocab_definition_list]
 
-  # Generate Vocab Definitions
-  vocab_defs = def_vocab(vocab_list, target_lang)
-  vocab_defs.strip()
-  vocab_definition_list = vocab_defs.split("#NEWDEF#")
-  vocab_definition_list = [entry.strip() for entry in vocab_definition_list]
+    with st.expander("Vocab definitions:"):
+      st.write(vocab_definition_list)
 
-  with st.expander("Vocab definitions:"):
-    st.write(vocab_definition_list)
+    # Generate Comprehension Questions
+    questions = comprehension_q(vid_transcript = str(transcript[0].page_content), target_language= target_lang)
+    
+    qa_list = questions.split('#QUESTION#')
 
-  # Generate Comprehension Questions
-  questions = comprehension_q(vid_transcript = str(transcript[0].page_content), target_language= target_lang)
-  
-  qa_list = questions.split('#QUESTION#')
+    qa_pairs = []
 
-  qa_pairs = []
+    for qa in qa_list:
+      if qa.strip() != "":
+        question, answer = qa.split("#ANSWER#")
+        qa_pairs.append({"question": question.strip(), "answer": answer.strip()})
+    
+    with st.expander("Questions about the video"):
+      for qa in qa_pairs:
+        st.write(qa["question"])
+      for qa in qa_pairs:
+        st.write(qa["answer"])
 
-  for qa in qa_list:
-    if qa.strip() != "":
-      question, answer = qa.split("#ANSWER#")
-      qa_pairs.append({"question": question.strip(), "answer": answer.strip()})
-  
-  with st.expander("Questions about the video"):
+    worksheet = Document()
+
+    # Define narrow margins
+    horizontal_margin = Inches(1)
+    vertical_margin = Inches(0.5)
+
+    # Set margins for each section in the document
+    for section in worksheet.sections:
+        section.top_margin = vertical_margin
+        section.bottom_margin = vertical_margin
+        section.left_margin = horizontal_margin
+        section.right_margin = horizontal_margin
+
+    paragraph_format = worksheet.styles['Normal'].paragraph_format
+    paragraph_format.space_after = None
+
+    worksheet.add_heading(title, level=1)
+    head = worksheet.paragraphs[-1]
+    head.alignment = WD_ALIGN_PARAGRAPH.CENTER
+    heading_format = head.paragraph_format
+    heading_format.space_before = Pt(0)  # Adjust space before heading
+    heading_format.space_after = Pt(5)  # Adjust space after heading
+
+    image_stream = download_image(thumbnail_url)
+    if image_stream:
+      worksheet.add_picture(image_stream, width=Inches(4))
+      thumb = worksheet.paragraphs[-1]
+      thumb.alignment = WD_ALIGN_PARAGRAPH.CENTER
+      thumb_format = thumb.paragraph_format
+      thumb_format.space_after = Pt(10)  # Adjust space after heading
+
+    worksheet.add_heading("Vocabulary", level=2)
+    table = worksheet.add_table(rows=4, cols=3)
+
+    # Populate the table with vocab
+    for i, vocab in enumerate(vocab_list):
+      row = i // 3
+      col = i % 3
+      paragraph = table.cell(row, col).paragraphs[0]
+      paragraph.add_run(f"{i+1}. {vocab}")
+
+    worksheet.add_heading("Match the Vocabulary to the Definitions:", level=2)
+    shuffled_definitions = vocab_definition_list[:]
+    random.shuffle(shuffled_definitions)
+    for definition in shuffled_definitions:
+      worksheet.add_paragraph(f"__________ - {definition}")
+
+    worksheet.add_heading("Comprehension Questions", level=2)
+
     for qa in qa_pairs:
-      st.write(qa["question"])
+      worksheet.add_paragraph(qa["question"])
+      answer_line1 = worksheet.add_paragraph('_' * 100)
+      answer_line2 = worksheet.add_paragraph('_' * 100)
+      answer_line_format = answer_line1.paragraph_format
+      answer_line_format.space_before = Pt(5)  # Adjust space before line
+      answer_line_format.space_after = Pt(10)  # Adjust space after line
+      answer_line_format = answer_line2.paragraph_format
+      answer_line_format.space_before = Pt(5)  # Adjust space before line
+      answer_line_format.space_after = Pt(10)  # Adjust space after line
+
+    worksheet.add_page_break()
+    worksheet.add_heading("Worksheet Answers", level=2)
+
+    for i in range(10):
+      worksheet.add_paragraph(f"{vocab_list[i]} - {vocab_definition_list[i]}")
+    
     for qa in qa_pairs:
-      st.write(qa["answer"])
+      question = qa["question"]
+      answer = qa["answer"]
+      question_answer = question + " " + answer
+      worksheet.add_paragraph(question_answer)
+    
+    bio = io.BytesIO()
+    worksheet.save(bio)
 
-  worksheet = Document()
-
-  # Define narrow margins
-  horizontal_margin = Inches(1)
-  vertical_margin = Inches(0.5)
-
-  # Set margins for each section in the document
-  for section in worksheet.sections:
-      section.top_margin = vertical_margin
-      section.bottom_margin = vertical_margin
-      section.left_margin = horizontal_margin
-      section.right_margin = horizontal_margin
-
-  paragraph_format = worksheet.styles['Normal'].paragraph_format
-  paragraph_format.space_after = None
-
-  worksheet.add_heading(title, level=1)
-  head = worksheet.paragraphs[-1]
-  head.alignment = WD_ALIGN_PARAGRAPH.CENTER
-  heading_format = head.paragraph_format
-  heading_format.space_before = Pt(0)  # Adjust space before heading
-  heading_format.space_after = Pt(5)  # Adjust space after heading
-
-  image_stream = download_image(thumbnail_url)
-  if image_stream:
-    worksheet.add_picture(image_stream, width=Inches(4))
-    thumb = worksheet.paragraphs[-1]
-    thumb.alignment = WD_ALIGN_PARAGRAPH.CENTER
-    thumb_format = thumb.paragraph_format
-    thumb_format.space_after = Pt(10)  # Adjust space after heading
-
-  worksheet.add_heading("Vocabulary", level=2)
-  table = worksheet.add_table(rows=4, cols=3)
-
-  # Populate the table with vocab
-  for i, vocab in enumerate(vocab_list):
-    row = i // 3
-    col = i % 3
-    paragraph = table.cell(row, col).paragraphs[0]
-    paragraph.add_run(f"{i+1}. {vocab}")
-
-  worksheet.add_heading("Match the Vocabulary to the Definitions:", level=2)
-  shuffled_definitions = vocab_definition_list[:]
-  random.shuffle(shuffled_definitions)
-  for definition in shuffled_definitions:
-    worksheet.add_paragraph(f"__________ - {definition}")
-
-  worksheet.add_heading("Comprehension Questions", level=2)
-
-  for qa in qa_pairs:
-    worksheet.add_paragraph(qa["question"])
-    answer_line1 = worksheet.add_paragraph('_' * 100)
-    answer_line2 = worksheet.add_paragraph('_' * 100)
-    answer_line_format = answer_line1.paragraph_format
-    answer_line_format.space_before = Pt(5)  # Adjust space before line
-    answer_line_format.space_after = Pt(10)  # Adjust space after line
-    answer_line_format = answer_line2.paragraph_format
-    answer_line_format.space_before = Pt(5)  # Adjust space before line
-    answer_line_format.space_after = Pt(10)  # Adjust space after line
-
-  worksheet.add_page_break()
-  worksheet.add_heading("Worksheet Answers", level=2)
-
-  for i in range(10):
-    worksheet.add_paragraph(f"{vocab_list[i]} - {vocab_definition_list[i]}")
-  
-  for qa in qa_pairs:
-    question = qa["question"]
-    answer = qa["answer"]
-    question_answer = question + " " + answer
-    worksheet.add_paragraph(question_answer)
-  
-  bio = io.BytesIO()
-  worksheet.save(bio)
+  st.success("Successfully Generated Worksheet")
 
   st.download_button(
       label="Download Worksheet",
